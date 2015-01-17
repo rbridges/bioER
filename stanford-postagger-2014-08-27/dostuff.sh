71485b41b3b8511b0bc44af2f61feaa16d35925c
Ryan Bridges <rybridges90@gmail.com>
2015-01-16 20:21:26 -0600

    merge

--- stanford-postagger-2014-08-27/dostuff.sh
+++
@@ -1,3 +0,0 @@
-java -mx300m -classpath stanford-postagger.jar edu.stanford.nlp.tagger.maxent.MaxentTagger -model models/english-left3words-distsim.tagger -textFile sample-input.txt > sample-tagged.txt
-python putLines.py
-python queryDB.py
